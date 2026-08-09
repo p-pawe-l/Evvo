a0155f0b62cee30e097b7161c2aa5fd81342af10
Pablo <pawelkozikowski58@gmail.com>
2026-08-09 14:28:42 +0200

    Added structe containg information about each generation

---
+++ include/generation_stats.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file generation_stats.hpp
+ * @brief Defines GenerationStats<T>, a snapshot of a single generation
+ *        passed to Evolver<T>'s callback and produced by EvoPolicy<T>.
+ */
+
+#pragma once
+
+#include <vector>
+
+#include "genome.hpp"
+
+/**
+ * @brief Snapshot of a single generation's evolution results.
+ * @tparam T Gene type of the genomes being evolved.
+ */
+template <typename T>
+struct GenerationStats {
+    int generation;
+    const std::vector<Genome<T>*>& population;
+    double best_fitness;
+    double avg_fitness;
+    Genome<T>* best_genome;
+};
