1e174a546e6e4b70d7bae936fa4e6469b1b5c188
Pablo <pawelkozikowski58@gmail.com>
2026-08-22 13:10:15 +0200

    Added header file for selection functions

---
+++ include/selection_func.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "genome.hpp"
+
+template <typename T>
+std::size_t roulette_selection(const PopulationVec<T>& population)
+{
+
+}
+
+template <typename T>
+std::size_t linear_rank_selection(const PopulationVec<T>& population)
+{
+
+}
+
+template <typename T>
+std::size_t exp_rank_selection(const PopulationVec<T>& population)
+{
+
+}
+
+template <typename T>
+std::size_t steady_state_selection(const PopulationVec<T>& population)
+{
+
+}
+
+template <typename T>
+std::size_t tournament_selection(const PopulationVec<T>& population)
+{
+
+}
+
+template <typename T>
+std::size_t truncation_selection(const PopulationVec<T>& population)
+{
+
+}
+
+template <typename T>
+std::size_t elitist_selection(const PopulationVec<T>& population)
+{
+
+}
+
+template <typename T>
+std::size_t boltzmann_selection(const PopulationVec<T>& population) 
+{
+
+}
+
+template <typename T>
+std::size_t lexicase_selection(const PopulationVec<T>& population)
+{
+    
+}
