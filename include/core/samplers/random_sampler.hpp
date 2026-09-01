46113a80b75145ea21f0b94cfd8c5bdf43aa8331
Pawel <pawelkozikowski58@gmail.com>
2026-09-02 00:37:50 +0200

    Updated: Removed docs for samplers - not necesseray to have

--- include/core/samplers/random_sampler.hpp
+++ include/core/samplers/random_sampler.hpp
@@ -1,28 +1,30 @@
 #pragma once
 
-#include "../population_init.hpp"
-#include "../../util/rand_util.hpp"
-#include <type_traits>  
 #include <random>
+#include <type_traits>
 
-template <typename SampleT> requires std::is_arithmetic_v<SampleT>
+#include "../../util/rand_util.hpp"
+#include "../population_init.hpp"
+
+template <typename SampleT>
+    requires std::is_arithmetic_v<SampleT>
 struct RandomSamplerParams {
     SampleT low_;
     SampleT high_;
 };
 
-template <typename SampleT> requires std::is_arithmetic_v<SampleT>
+template <typename SampleT>
+    requires std::is_arithmetic_v<SampleT>
 class RandomSampler : public Sampler<SampleT> {
 private:
     std::pair<SampleT, SampleT> range_;
 
 public:
-    explicit RandomSampler(const RandomSamplerParams& params): range_{params.low_, params.high_} {}
-    RandomSampler(SampleT low, SampleT high): range_{low, high} {}
+    explicit RandomSampler(const RandomSamplerParams<SampleT>& params)
+        : range_{params.low_, params.high_} {}
+    RandomSampler(SampleT low, SampleT high) : range_{low, high} {}
 
-    SampleT get_sample() const override {
-        SampleT rand_val = random_value<SampleT>(range_.first, range_.second);
-        return rand_val;
+    [[nodiscard]] SampleT get_sample() const noexcept override {
+        return random_value<SampleT>(range_.first, range_.second);
     }
-
 };
--- include/core/samplers/value_sampler.hpp
+++ include/core/samplers/value_sampler.hpp
@@ -1,17 +1,13 @@
-#pragma once 
+#pragma once
 
 #include "../population_init.hpp"
 
-template <typename SampleT>
-class ValueSampler : public Sampler<SampleT> {
+template <typename SampleT> class ValueSampler : public Sampler<SampleT> {
 private:
     SampleT val_;
-    
+
 public:
-    explicit ValueSampler(SampleT value): val_{value} {}
+    explicit ValueSampler(SampleT value) : val_{value} {}
 
-    SampleT get_sample() const noexcept override {
-        return val_;
-    } 
-
+    [[nodiscard]] SampleT get_sample() const noexcept override { return val_; }
 };
