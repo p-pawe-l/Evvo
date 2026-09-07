c67bdea1dcf86060453bdc4a7455eba143866315
Pawel <pawelkozikowski58@gmail.com>
2026-09-07 23:46:49 +0200

    Added two more tactics

--- include/core/fitness.hpp
+++ include/core/fitness.hpp
@@ -6,23 +6,44 @@
 namespace evvo::fitness::tactics {
     struct higher_better {
     private:
-        const double worst_fitness = -std::numeric_limits<double>::infinity();
+        constexpr static double worst_fitness = -std::numeric_limits<double>::infinity();
     
     public:
-        [[nodiscard]] double get_worst_fit() const noexcept { return worst_fitness; }
-        constexpr bool operator()(const double f1, const double f2) {
+        [[nodiscard]] constexpr static double get_worst_fit() noexcept { return worst_fitness; }
+        constexpr bool operator()(const double f1, const double f2) const noexcept {
             return f1 > f2;
         }
     };
 
-    struct lower_better  {
+    struct higher_or_equal_better {
     private:
-        const double worst_fitness = std::numeric_limits<double>::infinity();
+        constexpr static double worst_fitness = higher_better::get_worst_fit();
 
     public:
-        [[nodiscard]] double get_worst_fit() const noexcept { return worst_fitness; }
-        constexpr bool operator()(const double f1, const double f2) {
+        [[nodiscard]] constexpr static double get_worst_fit() noexcept { return worst_fitness; }
+        constexpr bool operator()(const double f1, const double f2) const noexcept {
+            return f1 >= f2;
+        }
+    };
+    struct lower_better  {
+    private:
+        constexpr static double worst_fitness = std::numeric_limits<double>::infinity();
+
+    public:
+        [[nodiscard]] constexpr static double get_worst_fit() noexcept { return worst_fitness; }
+        constexpr bool operator()(const double f1, const double f2) const noexcept {
             return f1 < f2;
+        }
+    };
+
+    struct lower_or_equal_better {
+    private:
+        constexpr static double worst_fitness = lower_better::get_worst_fit();
+
+    public:
+        [[nodiscard]] constexpr static double get_worst_fit() noexcept { return worst_fitness; }
+        constexpr bool operator()(const double f1, const double f2) const noexcept {
+            return f1 <= f2;
         }
     };
 }
