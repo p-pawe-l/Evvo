5cc87328280fd6814384b0db1410a01ac5cc0702
Pawel <pawelkozikowski58@gmail.com>
2026-09-07 11:54:10 +0200

    Added simple counter-based cache

---
+++ include/util/cache.hpp
@@ -0,0 +1,45 @@
+#pragma once 
+
+#include <unordered_map>
+#include <algorithm>
+
+template <typename T>
+struct CacheEntry {
+    T data;
+    std::size_t counter_ = 0;
+};
+
+template <typename Key, typename Value> 
+class Cache {
+private:
+    std::size_t cap_;
+    std::unordered_map<Key, CacheEntry<Value>> hash_map_;
+
+    void remove_min_counter() {
+        auto min_item = std::ranges::min(
+            hash_map_,
+            [](const std::pair<Key, CacheEntry<Value>> item) {
+                return item.second.counter_;
+            }
+        );
+        hash_map_.erase(std::move(min_item.second));
+    } 
+
+public:
+    Cache() = delete;
+    explicit Cache(std::size_t cap): cap_{cap} {}
+
+    Value get(Key key) const {
+        return hash_map_.at(key);
+    }
+
+    void put(const Key& key, const Value& val) {
+        if (hash_map_.size() == cap_) { remove_min_counter(); }
+        hash_map_.insert({key, val});    
+    }
+
+    void put(const Key& key, Value&& val) {
+        if (hash_map_.size() == cap_) { remove_min_counter(); }
+        hash_map_.insert({key, val});
+    }
+};
