a0d68a07ee9c8187d40573228770832a8e515841
Pawel <pawelkozikowski58@gmail.com>
2026-09-09 11:26:20 +0200

    Added Id generator in genome.hpp

--- include/core/genome.hpp
+++ include/core/genome.hpp
@@ -1,36 +1,49 @@
 #pragma once
 
+#include <atomic>
 #include <cstddef>
 #include <type_traits>
 #include <vector>
 
 namespace evvo::genome {
     
-    template <typename T> 
-    using Genome = std::vector<T>;
+template <typename T> 
+using Genome = std::vector<T>;
 
-    template <typename T, typename Generator> requires std::is_arithmetic_v<T>
-    Genome<T> make_genome(const std::size_t len, Generator&& gene_gen) {
-        Genome<T> genome;
-        genome.reserve(len);
-        for (std::size_t i = 0; i < len; ++i) { genome.push_back(gene_gen()); }
-        return genome;
+template <typename T, typename Generator> requires std::is_arithmetic_v<T>
+Genome<T> make_genome(const std::size_t len, Generator&& gene_gen) {
+    Genome<T> genome;
+    genome.reserve(len);
+    for (std::size_t i = 0; i < len; ++i) { genome.push_back(gene_gen()); }
+    return genome;
+}
+
+template <typename T>
+using Population = std::vector<Genome<T>>;
+struct PopulationMeta {
+    std::size_t population_size;
+    std::size_t genome_size;
+};
+
+template <typename T, typename Generator> requires std::is_arithmetic_v<T>
+Population<T> make_population(const PopulationMeta& arg, Generator&& gene_gen) {
+    Population<T> population;
+    population.reserve(arg.population_size);
+    for (std::size_t i = 0; i < arg.population_size; ++i) {
+        population.push_back(make_genome<T>(arg.genome_size, gene_gen));
     }
+    return population;
+}
 
-    template <typename T>
-    using Population = std::vector<Genome<T>>;
-    struct PopulationMeta {
-        std::size_t population_size;
-        std::size_t genome_size;
-    };
-
-    template <typename T, typename Generator> requires std::is_arithmetic_v<T>
-    Population<T> make_population(const PopulationMeta& arg, Generator&& gene_gen) {
-        Population<T> population;
-        population.reserve(arg.population_size);
-        for (std::size_t i = 0; i < arg.population_size; ++i) {
-            population.push_back(make_genome<T>(arg.genome_size, gene_gen));
-        }
-        return population;
+// Thread safe generataion id
+class GenomeIdGenerator {
+private:
+    std::atomic<std::uint64_t> next_id_{0};
+    
+public:
+    std::uint64_t allocate_id() {
+        return next_id_.fetch_add(1, std::memory_order_relaxed);
     }
+};
+
 }
