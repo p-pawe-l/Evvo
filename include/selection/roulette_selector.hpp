78eaa67903aa99493e248dc8808a3a0a04d9b922
Pawel <pawelkozikowski58@gmail.com>
2026-09-10 11:00:46 +0200

    Updated selector classes

--- include/selection/roulette_selector.hpp
+++ include/selection/roulette_selector.hpp
@@ -1,18 +1,24 @@
 #pragma once
 
-#include "../core/evo_policy.hpp"
 #include "selector.hpp"
 
-// Builds a cumulative fitness array once at construction, then picks an
-// individual with probability proportional to its share of total fitness
-// in O(log n) per pick. Requires fitnesses to be non-negative.
-class RouletteSelector : public Selector<PopulationEval> {
+#include "../policy/evo_policy.hpp"
+#include "../core/fitness.hpp"
+#include "../core/eval.hpp"
+
+namespace evvo::selection {
+
+template <evvo::fitness::tactics::concepts::FitnessTactic Tactics>
+    requires std::is_default_constructible_v<Tactics>
+class RouletteSelector : public evvo::selection::AbstractSelector<evvo::eval::PopulationEval<Tactics>> {
 private:
     double total_fitness_;
     const std::vector<double>* fitnesses_;
 
 public:
-    void build_from_eval(const PopulationEval& eval) override;
+    void build_from_eval(const evvo::eval::PopulationEval<Tactics>& eval) override;
 
     [[nodiscard]] std::size_t pick() const override;
 };
+
+}
--- include/selection/selector.hpp
+++ include/selection/selector.hpp
@@ -2,16 +2,14 @@
 
 #include <cstddef>
 
-// Implementations do their one-time preparation (sorting, building a
-// cumulative fitness array, etc.) in build_from_eval(), so repeated
-// pick() calls across a generation stay cheap.
-template <typename EvalT> class Selector {
+namespace evvo::selection {
+
+template <typename EvalT> 
+class AbstractSelector {
 public:
-    virtual ~Selector() = default;
-
+    virtual ~AbstractSelector() = default;
     virtual void build_from_eval(const EvalT& eval) = 0;
-
-    // Returns the index of the selected individual into the population
-    // the selector was constructed from.
     [[nodiscard]] virtual std::size_t pick() const = 0;
 };
+
+}
